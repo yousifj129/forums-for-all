f4d0b58561a43da9ef8fd524203271ee0e15dc4e
yousifj129 <yousifjaffar129@gmail.com>
2025-09-03 10:43:15 +0300

    added homepage and fixed sign up form

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("auth/signup/", views.SignUpView.as_view() , name="signup")
+   path("", views.HomePageView.as_view() , name="homepage"),
+   path("auth/signup/", views.SignUpView.as_view() , name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView, CreateView
 from .forms import UserSignUpForm
-class SignUpView(FormView):
+class SignUpView(CreateView):
     template_name = "registration/signup.html"
     form_class = UserSignUpForm
     success_url = reverse_lazy("login")  # or your home
+
+        
+    
+
+class HomePageView(TemplateView):
+    template_name = "homepage.html"
+    
