3bc4cc6321bf207abcc30f4546af3209dddd2214
yousifj129 <yousifjaffar129@gmail.com>
2025-09-11 09:33:53 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Forums For All
--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Forum, Upvote, Downvote, Comment,User, Attachment
+from .models import Forum, Upvote, Downvote, Comment,User, Attachment, Follow
 # Register your models here.
 admin.site.register(Forum)
 admin.site.register(Upvote)
@@ -7,3 +7,4 @@ admin.site.register(Downvote)
 admin.site.register(Comment)
 admin.site.register(User)
 admin.site.register(Attachment)
+admin.site.register(Follow)
